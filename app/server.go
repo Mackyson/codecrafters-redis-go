6777dacbe0897af4a4d23bb85b367f9db5851d56
Mackyson <27495193+Mackyson@users.noreply.github.com>
2023-01-06 13:33:42 +0900

    response concurrently

--- app/server.go
+++ app/server.go
@@ -15,12 +15,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go func(conn net.Conn) {
+			_, err = conn.Write([]byte("+PONG\r\n"))
+		}(conn)
+
 	}
-
-	_, err = conn.Write([]byte("+PONG\r\n"))
-
 }
